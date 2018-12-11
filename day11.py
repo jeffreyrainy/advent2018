4f0da52226b1be8eae6618fd2205015fa3269452
Jeffrey Rainy <jeff@elementai.com>
2018-12-11 16:39:16 -0500

    improved performance

--- day11.py
+++ day11.py
@@ -1,20 +1,15 @@
-import queue
-import math
 import numpy as np
-import copy
 
-# this could be massively improved.
-# but it gave the answer fast enough.
-def process(values, r):
+def process(sum_corner, r, size):
     best = 0
-    best_coord = None
     for z in range(r[0], r[1]):
-        for x in range(300 - (z-1)):
-            for y in range(300 - (z-1)):
-                v = np.sum(values[x:x+z,y:y+z])
+        for x in range(size - z):
+            for y in range(size - z):
+                v = sum_corner[(x + z,y + z)] - sum_corner[x + z, y] - sum_corner[x, y + z] + sum_corner[x, y]
+
                 if v > best:
                     best = v
-                    best_coord = (x,y,z)
+                    best_coord = (x + 1, y + 1, z)
 
     print(best_coord)
 
@@ -24,15 +19,26 @@ def main():
     serial = int(file.readline())
     print(serial)
 
-    values = np.zeros((300,300))
+    size = 300
 
-    for x in range(300):
-        for y in range(300):
+    values = np.zeros((size,size), dtype=int)
+
+    for x in range(size):
+        for y in range(size):
             v = ((x + 10) * y + serial) * (x + 10)
             values[(x,y)] = (v // 100) % 10 - 5
 
-    process(values, [3, 4])
-    process(values, [0, 300])
+    # the sum from the (0,0) corner to a given position
+    sum_corner = np.zeros((size,size), dtype=int)
+
+    for x in range(size):
+        for y in range(size):
+            sum_corner[(x,y)] = sum_corner[(x-1,y)] + sum_corner[(x,y - 1)] - sum_corner[(x - 1,y - 1)] + values[(x,y)]
+
+    process(sum_corner, [3,4], size)
+    process(sum_corner, [0,size], size)
+
+
 
 if __name__ == "__main__":
     main()
