9bf2a2ed8a6800f1101497523417e6b36a5ef68f
Jeffrey Rainy <jeff@elementai.com>
2018-12-14 11:54:25 -0500

    day 12, part 2

--- day12.py
+++ day12.py
@@ -1,13 +1,24 @@
 import numpy as np
 
-left = 0
+def trim(value, pos):
 
-def process(tape, rules):
-    global left
+    i = 0
+    while value[i] == '.':
+        i += 1
+        pos += 1
+    value = value[i:]
 
-    if '.' in tape[0:2] or '.' in tape[-2, 0]:
-        tape = ['.', '.'] + tape + ['.', '.']
-        left -= 2
+    i = -1
+    if value[i] == '.':
+        while value[i] == '.':
+            i -= 1
+        value = value[:i+1]
+
+    return value, pos
+
+def process(it, tape, rules, left):
+    tape = ['.', '.', '.'] + tape + ['.', '.', '.']
+    left -= 3
     ret = tape[:]
 
     for i in range(2, len(tape) - 2):
@@ -15,9 +26,36 @@ def process(tape, rules):
         if key in rules:
             ret[i] = rules[key]
 
-    return ret
+    ret, left = trim(ret,left)
+    return ret, left
+
+def run(tape, rules):
+    left = 0
+    iterations = 50000000000
+    predicted = False
+
+    for i in range(iterations):
+        prev, prev_left = tape[:], left
+        tape, left = process(i, tape, rules, left)
+
+        if not predicted and prev == tape:
+            pred_left = left + (iterations - i) * (left - prev_left) - 1
+
+            print("Same value again. i is {}, shift is {}, predicted shift is {} ".format(i,left - prev_left, pred_left))
+            predicted = True
+            break
+
+    if predicted:
+        left = pred_left
+    count = 0
+    for x in range(len(tape)):
+        if tape[x] == '#':
+            count += (x + left)
+
+    print(count)
 
 def main():
+    global do_trim
     file = open("inputs/input12.txt")
 
     tape = list(file.readline().split()[2])
@@ -29,22 +67,7 @@ def main():
             pattern, result = line[0], line[2]
             rules[pattern] = result
 
-
-    iterations = 20
-
-#    print(''.join(tape))
-    for i in range(iterations):
-        tape = process(tape, rules)
-#        print(''.join(tape))
-
-        if i % 100 == 0:
-            print(i)
-    count = 0
-    for x in range(len(tape)):
-        if tape[x] == '#':
-            count += (x + left)
-
-    print(count)
+    run(tape, rules)
 
 
 if __name__ == "__main__":
